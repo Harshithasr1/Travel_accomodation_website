287bc9d94b649926915461324890b8891e79060c
Harshithasr1 <109043245+Harshithasr1@users.noreply.github.com>
2023-06-17 20:39:31 -0500

    first commit

---
+++ src/app/emart/bill/bill-list/bill-list.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { EmartService } from '../../emart.service';
+
+
+@Component({
+  selector: 'app-bill-list',
+  templateUrl: './bill-list.component.html',
+  styleUrls: ['./bill-list.component.css']
+})
+export class BillListComponent implements OnInit {
+  allBills: any;
+  currentBuyer: any;
+  showBill: boolean;
+  currentUser: any;
+  mileage: number;
+  constructor(protected emartService: EmartService) {
+   }
+  ngOnInit(): void {
+    this.currentBuyer=  JSON.parse(localStorage.getItem('i1'));//retrieving current buyer details from local storage  
+    this.allBills = [];
+    this.allBills =  JSON.parse(localStorage.getItem('i2'));//retrieving all bills from local storage
+    if (this.allBills.length == 0) {
+      this.showBill = false;
+    }
+    else {
+      this.showBill = true;
+    }
+
+    this.emartService.getBuyer()
+      .subscribe(
+        (response) => {
+          this.currentUser = response;
+        }
+      );
+
+  }
+}
